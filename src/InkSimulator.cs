13db87d762f2b3fa2f9d8feb6f44f09930a59e9c
Abhinav Tushar <lepisma@fastmail.com>
2019-10-14 23:29:39 +0530

    dummy simulator

--- src/InkSimulator.cs
+++ src/InkSimulator.cs
@@ -1,21 +1,27 @@
+using Ink;
 using Ink.Runtime;
 using System;
 
-class InkSimulator {
-
-    static void runStory(Story story) {
-        while (story.canContinue) {
+class InkSimulator
+{
+    static void runStory(Story story)
+    {
+        while (story.canContinue)
+        {
             Console.WriteLine(story.Continue() + " :tags " + String.Join(" ", story.currentTags));
         }
     }
 
-    static void Main(string[] args) {
-        string text = System.IO.File.ReadAllText(args[0]);
-        Story story = new Story(text);
+    static void Main(string[] args)
+    {
+        var ifc = new InkFlowCompiler();
+        var text = ifc.CompileFile(args[0]);
+        var story = new Story(text);
 
         runStory(story);
 
-        for (int i = 0; i < story.currentChoices.Count; ++i) {
+        for (int i = 0; i < story.currentChoices.Count; ++i)
+        {
             Choice choice = story.currentChoices [i];
             Console.WriteLine("Choice " + (i + 1) + ". " + choice.text);
         }
